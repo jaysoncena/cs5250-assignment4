09a1b410cd41891051e01c4b1b4b2b72fd5119e6
Jayson Cena <jayson@cloudflare.com>
2019-04-17 07:10:50 +0800

    implementation of SRTF

--- asg4.py
+++ asg4.py
@@ -1,21 +1,146 @@
 import sys
 
-from collections import deque
+from collections import deque, MutableSequence
 
 class Process:
-    last_scheduled_time = 0
-    def __init__(self, id, arrive_time, burst_time):
+    def __init__(self, id, arrive_time, burst_time, last_preempt_time=0):
         self.id = id
         self.arrive_time = arrive_time
         self.burst_time = burst_time
+        self.last_preempt_time = last_preempt_time
+        self.total_waiting_time = 0
     #for printing purpose
     def __repr__(self):
         # return ('[id %d : arrival_time %d,  burst_time %d]'%(self.id, self.arrive_time, self.burst_time))
-        return ("id={}, arrival_time={}, burst_time={}".format(self.id, self.arrive_time, self.burst_time))
+        return ("id={}, arrival_time={}, burst_time={}, last_preempt_time={}".format(
+            self.id,
+            self.arrive_time,
+            self.burst_time,
+            self.last_preempt_time))
+    def last_process_time(self):
+        return max([self.arrive_time, self.last_preempt_time])
 
+
+class SRTFList(MutableSequence):
+    def __init__(self, data=None):
+        super(SRTFList, self).__init__()
+        if data is None:
+            self._list = list()
+        else:
+            self._list = list(data)
+        
+        self._sorted()
+    def __repr__(self):
+        ret = ["{0} {1}".format(type(x), x) for x in self._list]
+        return "\n".join(ret)
+    def __len__(self):
+        return len(self._list)
+    def __getitem__(self, ii):
+        return self._list[ii]
+    def __delitem__(self, ii):
+        del self._list[ii]
+    def __setitem__(self, ii, val):
+        self._list[ii] = val
+    def __str__(self):
+        return self.__repr__()
+    def insert(self, val):
+        pass
+    def append(self, val):
+        pass
+
+    def add(self, val):
+        if not isinstance(val, Process):
+            raise ValueError("<{0} {1}> is not an instance of {2}".format(val.__class__.__name__, val, Process.__class__.__name__))
+
+        self._list.insert(len(self._list), val)
+        self._sorted()
+
+    def _sorted(self):
+        self._list = sorted(self._list, key=lambda x: x.burst_time)
+
+    def _sorted_by_process_time(self):
+        self._list = sorted(self._list, key=lambda x: x.last_process_time())
+
+    def get_next_arrive_time(self):
+        """
+            return the closest `arrive_time` from
+            the given `current_time` argument
+        """
+        if len(self._list) > 0:
+            self._sorted_by_process_time()
+            return self._list[0].last_process_time()
+        # else
+        return -1
+
+    def get(self):
+        """ gets the process with SRTF """
+        pass
+
+    # def pop_left_by_arrive_time(self):
+    #     if self._list:
+    #         self._sorted_by_arrive_time()
+    #         p = self._list[0]
+    #         del self._list[0]
+    #         return p
+    #     else:
+    #         return None
+    def get_by_srtf(self, current_time, pop=False):
+        if len(self._list) < 1:
+            return None
+        elif len(self._list)  == 1:
+            # pop
+            p = self._list[0]
+            if pop:
+                del self._list[0]
+            return p
+        # else
+        filtered_list = [x for x in self._list if x.last_process_time() <= current_time]
+        if not filtered_list:
+            self._sorted_by_process_time()
+            # pop
+            p = self._list[0]
+            if pop:
+                del self._list[0]
+            return p
+
+        # least burst_time
+        filtered_list = sorted(filtered_list, key=lambda x: x.burst_time)
+        # pop
+        p_num = self._list.index(filtered_list[0])
+        p = self._list[p_num]
+        if pop:
+            del self._list[p_num]
+        return p
+
+    def pop_by_srtf(self, current_time):
+        if len(self._list) < 1:
+            return None
+        elif len(self._list)  == 1:
+            # pop
+            p = self._list[0]
+            del self._list[0]
+            return p
+        # else
+        filtered_list = [x for x in self._list if x.last_process_time() <= current_time]
+        if not filtered_list:
+            self._sorted_by_process_time()
+            # pop
+            p = self._list[0]
+            del self._list[0]
+            return p
+
+        # least burst_time
+        filtered_list = sorted(filtered_list, key=lambda x: x.burst_time)
+        # pop
+        p_num = self._list.index(filtered_list[0])
+        p = self._list[p_num]
+        del self._list[p_num]
+        return p
+    
 
 def rr(process_list, quantum=2):
     current_time = 0
+    waiting_time = 0
     queue = deque(process_list)
 
     while queue:
@@ -23,6 +148,8 @@ def rr(process_list, quantum=2):
 
         if current_time < p.arrive_time:
             current_time = p.arrive_time
+
+        waiting_time += current_time - p.last_process_time()
 
         yield [current_time, p]
 
@@ -35,8 +162,72 @@ def rr(process_list, quantum=2):
             current_time += p.burst_time
             p.burst_time = 0
 
+        # after all logics are finished, we set the last current_time
+        # the process was run
+        p.last_preempt_time = current_time
+
         if p.burst_time > 0:
             queue.append(p)
+
+def srtf(process_list):
+    # processes_srtf = SRTFList()
+    # for p in process_list:
+    #     processes_srtf.add(p)
+    #     print("## add: {}".format(p))
+    #     print("## entries\n{}".format(processes_srtf))
+    # # for item in process_list:
+    # #     processes.add(item)
+    # #     print("## add: {}".format(item))
+    # #     print("## entry\n{}".format(processes))
+
+    # print(processes_srtf.get_next_arrive_time(0))
+    # print("## entries\n{}".format(processes_srtf))
+    proc_srtf = SRTFList(process_list)
+    current_time = 0
+    waiting_time = 0
+    force_next_p = None
+
+    while proc_srtf:
+        # workaround to bypass srtf selection
+        # and continue on the current process
+        # this will only be triggered when
+        # current p and next_p has the same burst_time
+        p = None
+        if force_next_p:
+            p = force_next_p
+            force_next_p = None
+        else:
+            p = proc_srtf.get_by_srtf(current_time, pop=True)
+
+        if current_time < p.arrive_time:
+            current_time = p.arrive_time
+
+        yield [current_time, p]
+
+        waiting_time += current_time - p.last_process_time()
+        next_arrive_time = proc_srtf.get_next_arrive_time()
+        # gets the next process without removing from list
+        next_p = proc_srtf.get_by_srtf(current_time, pop=False)
+        if next_p is not None and p.burst_time == next_p.burst_time:
+            force_next_p = p
+        
+
+        if next_arrive_time < 0:
+            continue # should quit the loop since this means no more items in list
+
+        p.burst_time -= next_arrive_time
+        current_time += next_arrive_time
+
+        if p.burst_time < 0:
+            diff = abs(p.burst_time)
+            p.burst_time = 0
+            current_time -= diff
+
+        p.last_preempt_time = current_time
+        if p.burst_time > 0:
+            proc_srtf.add(p)
+
+
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,41 +1,60 @@
 import asg4
 
-def main():
-    rr_test1()
+test_data1 = [
+    asg4.Process(0, 0, 9),
+    asg4.Process(1, 1, 2),
+    asg4.Process(2, 2, 1)
+]
 
-def rr_test1():
-    test_data1 = [
-        asg4.Process(0, 0, 9),
-        asg4.Process(1, 1, 2),
-        asg4.Process(2, 2, 1)
-    ]
+test_data2 = [
+    asg4.Process(0, 0, 7),
+    asg4.Process(1, 1, 6),
+    asg4.Process(2, 2, 9),
+    asg4.Process(3, 3, 10)
+]
 
-    test_data2 = [
-        asg4.Process(0, 0, 7),
-        asg4.Process(1, 1, 6),
-        asg4.Process(2, 2, 9),
-        asg4.Process(3, 3, 10)
-    ]
+test_data3 = [
+    asg4.Process(0, 0, 9),
+    asg4.Process(1, 5, 2),
+    asg4.Process(2, 6, 1)
+]
 
-    test_data3 = [
-        asg4.Process(0, 0, 9),
-        asg4.Process(1, 5, 2),
-        asg4.Process(2, 6, 1)
-    ]
+test_data4 = [
+    asg4.Process(0, 0, 1)
+]
 
-    test_data4 = [
-        asg4.Process(0, 0, 1)
-    ]
+# testing wait time
+test_data3 = [
+    asg4.Process(0, 0, 1),
+    asg4.Process(1, 5, 3),
+    asg4.Process(2, 6, 2)
+]
 
-    # testing wait time
-    test_data3 = [
-        asg4.Process(0, 0, 1),
-        asg4.Process(1, 5, 3),
-        asg4.Process(2, 6, 2)
-    ]
+# from lecture
+test_data4 = [
+    asg4.Process(1, 0, 24),
+    asg4.Process(2, 0, 3),
+    asg4.Process(3, 0, 3)
+]
 
-    for line in asg4.rr(test_data3):
+# simulate out-of-order burst_time
+test_data5 = [
+    asg4.Process(0, 0, 9),
+    asg4.Process(1, 1, 2),
+    asg4.Process(2, 2, 1),
+    asg4.Process(3, 3, 12),
+]
+
+
+def srtf_test():
+    for line in asg4.srtf(test_data5):
+        print(line)
+    # asg4.srtf(test_data5)
+    # for line in
+
+def rr_test():
+    for line in asg4.rr(test_data4, quantum=4):
         print(line)
 
 if __name__ == "__main__":
-    main()
+    srtf_test()
